c598d702128e651885bf0c73061126cbc9bcbeac
agsedov <agsedov@github.io>
2018-12-08 15:10:12 +0300

    random pick

--- list.js
+++ list.js
@@ -2,6 +2,60 @@
 
 const e = React.createElement;
 
+class RandomPicker {
+  constructor(list) {
+    this.list = list;
+  }
+
+  getHalf(i) {
+    return this.list.filter(e => e.h === i);
+  }
+
+  pickRandom(ar) {
+    var l = ar.length;
+    var n = Math.floor((Math.random() * l));
+    return ar[n];
+  }
+  pickThemeAndTheorem(i) {
+    var half = this.getHalf(i);
+    var theorems = [];
+    half.forEach((t)=>{
+      t.items.forEach((i)=>{
+        if(i.difficulty == 'hard' || i.difficulty == 'moderate') {
+          theorems.push(i.name);
+        }
+      });
+    });
+    
+    var theme = this.pickRandom(half);
+    var themeName = theme.name;
+    var items;
+    if(theme.items.length > 9) { //Если в теме больше 9 вопросов - поделить на 2
+      var newL = Math.ceil(theme.items.length/2);
+      var pickFromBegin = (Math.random() > 0.5);
+      if(pickFromBegin) {
+        items = theme.items.slice(newL);
+      } else {
+        items = theme.items.slice(-newL);
+      }
+    } else {
+      items = theme.items;
+    }
+    return {
+      "name" : theme.name,
+      "items": items,
+      "theorem": this.pickRandom(theorems)
+    }
+  }
+
+  pickTwoThemes() {
+    var p1 = this.pickThemeAndTheorem(1);
+    var p2 = this.pickThemeAndTheorem(2);
+    console.log(p1);
+    console.log(p2);
+  }
+}
+
 class Question extends React.Component {
     constructor(props) {
         super(props);
@@ -55,15 +109,24 @@ class ThemeList extends React.Component {
   componentDidUpdate() {
     MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
   }
+
+  regen(){
+    var r = new RandomPicker(this.state.list);
+    r.pickTwoThemes();
+  }
   render() {
     if(this.state.list) {
-        return e(
+        return [
+        e(
             'div',
-            { onClick: () => this.setState({ liked: true }) },
+            { onClick: () => this.regen() }, 'Случайный билет'
+        ),
+        e(
+            'div', {},
             this.state.list.map((theme)=>
                 e(Theme,{theme: theme})
             )
-        );
+        )];
     } else {
         return e(
                 'div',{},'Загрузка'
